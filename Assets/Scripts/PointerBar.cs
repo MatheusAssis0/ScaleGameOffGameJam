19c4cd030eb628d0a20b920c4b14a119a7b8e3bf
Matheus Assis <math.assis91@gmail.com>
2023-11-07 15:57:54 -0300

    StrenghBar Pointer

---
+++ Assets/Scripts/PointerBar.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerBar : MonoBehaviour
+{
+    [SerializeField] private GameObject pointer;
+    [SerializeField] private float speed, bounds;
+    public bool toLeft, toRight;
+    
+
+    private void Start()
+    {
+        toLeft = false;
+        toRight = true;
+    }
+
+    private void Update()
+    {
+        MoveThePointer();
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            toLeft = false;
+            toRight = false;
+        }
+    }
+
+    private void MoveThePointer()
+    {
+        Vector3 targetRight = new Vector3(bounds, pointer.transform.position.y, pointer.transform.position.z);
+        Vector3 targetLeft = new Vector3(-bounds, pointer.transform.position.y, pointer.transform.position.z);
+
+        if (toRight && !toLeft)
+        {
+            pointer.transform.position = Vector3.Lerp(pointer.transform.position, targetRight, speed * Time.deltaTime);
+            if (Vector3.Distance(pointer.transform.position, targetRight) <= 0.001f)
+            {
+                toLeft = true;
+                toRight = false;
+            }
+        }
+        else if(toLeft && !toRight)
+        {
+            pointer.transform.position = Vector3.Lerp(pointer.transform.position, targetLeft, speed * Time.deltaTime);
+            if (Vector3.Distance(pointer.transform.position, targetLeft) <= 0.001f)
+            {
+                toLeft = false;
+                toRight = true;
+            }
+        }
+    }
+}
